2bdb6ebbf13bf5bf75c11270a1665e4491b924cb
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-16 02:12:32 -0400

    more annotations

--- src/TodoDispatcher.hs
+++ src/TodoDispatcher.hs
@@ -1,7 +1,17 @@
 module TodoDispatcher (dispatchTodo) where
 
-import React.Flux
-import TodoStore
 
+
+import           React.Flux
+import           TodoStore
+
+
+
+-- * data SomeStoreAction = forall storeData. (StoreData storeData, NFData (StoreAction storeData))
+-- *    => SomeStoreAction (ReactStore storeData) (StoreAction storeData)
+-- *
+-- * instance NFData SomeStoreAction where
+-- *    rnf (SomeStoreAction _ action) = action `deepseq` ()
+--
 dispatchTodo :: TodoAction -> [SomeStoreAction]
 dispatchTodo a = [SomeStoreAction todoStore a]
